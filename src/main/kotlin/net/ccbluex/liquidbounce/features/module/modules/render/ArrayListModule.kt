f09e0c6345fb7067ca050fa0ec867f457a60e6d8
xiaoDao776 <3884075394@qq.com>
2026-08-09 20:48:48 +0800

    Update ArrayListModule.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ArrayListModule.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ArrayListModule.kt
@@ -4,12 +4,12 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.render
 
+import net.ccbluex.liquidbounce.config.types.*
 import net.ccbluex.liquidbounce.event.events.Render2DEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleManager
-import net.ccbluex.liquidbounce.features.value.*
 import net.ccbluex.liquidbounce.utils.client.mc
 import net.minecraft.client.gui.GuiGraphicsExtractor
 import java.awt.Color
