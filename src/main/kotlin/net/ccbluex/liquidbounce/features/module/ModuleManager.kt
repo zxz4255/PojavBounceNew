016e857a60d4fbdc224990c04f7cf6f66e01df51
xiaoDao776 <3884075394@qq.com>
2026-08-10 20:43:59 +0800

    Update ModuleManager.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -239,6 +239,7 @@ import net.ccbluex.liquidbounce.features.module.modules.render.hats.ModuleHats
 import net.ccbluex.liquidbounce.features.module.modules.render.hitfx.ModuleHitFX
 import net.ccbluex.liquidbounce.features.module.modules.render.murdermystery.ModuleMurderMystery
 import net.ccbluex.liquidbounce.features.module.modules.render.nametags.ModuleNametags
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleArrayList
 import net.ccbluex.liquidbounce.features.module.modules.render.trajectories.ModuleTrajectories
 import net.ccbluex.liquidbounce.features.module.modules.world.ModuleAirPlace
 import net.ccbluex.liquidbounce.features.module.modules.world.ModuleAutoDisable
@@ -676,6 +677,7 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleCrystalView,
             ModuleSkinChanger,
             ModuleProtectionZones,
+            ModuleArrayList,
             ModuleCrosshair,
 
             // World
