3c954ed92c59b7592692ca21ec3a10a14455dede
xiaoDao776 <3884075394@qq.com>
2026-08-13 09:16:31 +0800

    Update ModuleManager.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -90,7 +90,6 @@ import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleSkinDerp
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleTwerk
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleSolsticeNotifications
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleVomit
-import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleSolsticeArraylist
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.notebot.ModuleNotebot
 import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleAntiCheatDetect
 import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleAntiStaff
@@ -526,7 +525,6 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleSkinDerp,
             ModuleSolsticeNotifications,
             ModuleHandDerp,
-            ModuleSolsticeArraylist,
             ModuleTwerk,
             ModuleVomit,
 
