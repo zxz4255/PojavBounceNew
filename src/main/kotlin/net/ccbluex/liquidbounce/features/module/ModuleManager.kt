afcdddd614f3e124f183b71782112a1d0b4f1886
xiaoDao776 <3884075394@qq.com>
2026-08-13 08:08:46 +0800

    Update ModuleManager.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -88,9 +88,9 @@ import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleDerp
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleHandDerp
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleSkinDerp
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleTwerk
-import net.ccbluex.liquidbounce.features.module.modules.`fun´.ModuleSolticeNotifications
+import net.ccbluex.liquidbounce.features.module.modules.`fun´.ModuleSolsticeNotifications
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.ModuleVomit
-import net.ccbluex.liquidbounce.features.module.modules.`fun´.ModuleSolticeArraylist
+import net.ccbluex.liquidbounce.features.module.modules.`fun´.ModuleSolsticeArraylist
 import net.ccbluex.liquidbounce.features.module.modules.`fun`.notebot.ModuleNotebot
 import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleAntiCheatDetect
 import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleAntiStaff
@@ -524,9 +524,9 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleDerp,
             ModuleNotebot,
             ModuleSkinDerp,
-            MofuleSolticeNotifications,
+            MofuleSolsticeNotifications,
             ModuleHandDerp,
-            ModuleSolticeArraylist,
+            ModuleSolsticeArraylist,
             ModuleTwerk,
             ModuleVomit,
 
